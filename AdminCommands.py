ca824c0bd0ba098e162f3bdc4157592e42d0ea3c
Dreggor <dreggor@hammergaming.com>
2015-10-16 12:42:08 -0400

    Added phrases to keywords

--- AdminCommands.py
+++ AdminCommands.py
@@ -27,6 +27,7 @@ def word(client, message, db):
 		keywords = str(', '.join(keywords))
 		client.send_message(message.channel, keywords )
 		return()
+
 	if commandline[1] == "add":
 		commandsplit = str(message.content.split("add ",1)[1])
 		command = commandsplit.split(",")
@@ -40,25 +41,16 @@ def word(client, message, db):
 		except:
 			client.send_message(message.channel, 'Unable to add keyword: %s' % command[0])
 		return()
+
 	if commandline[1] == "remove":
-#		cur.execute("SELECT * FROM links")
-#		rows = cur.fetchall()
 		command = str(message.content.split("remove ",1)[1])
-#		keywords = []
-#		for i in rows:
-#		keywords.append(str(i[0]))
-#		if command in keywords:
-#		try:
 		print command
 		client.send_message(message.channel, "Deleting Keyword: " + command)
 		print("User: " + message.author.name + "Deleted keyword: " + command + " At: " + str(st))
 		cur.execute("DELETE FROM links WHERE word = ?", (command,))
 		db.commit()
-#		except:
-#			client.send_message(message.channel, 'Unable to delete keyword: %s' % command)
-#		else:
-#			client.send_message(message.channel, "Keyword %s not found." % command)
 		return()
+
 	if commandline[1] == "help":
 		client.send_message(message.channel, "Word command usage:")
 		client.send_message(message.channel, "Command: !word list  -Lists all active keywords-")
